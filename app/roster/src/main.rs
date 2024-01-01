1b74cf3b5c019bb21396607a665063c5a1e2dd3b
Anthony Griffon <anthony@griffon.one>
2024-01-01 18:37:55 +0100

    feat: add logging with tracing
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- app/roster/src/main.rs
+++ app/roster/src/main.rs
@@ -1,10 +1,16 @@
 mod infrastructure;
+
 use infrastructure::config::Cfg;
+use infrastructure::instruments::Instruments;
+use tracing::info;
 
 fn main() -> anyhow::Result<()> {
     // Initialize config
     let _config = Cfg::from_env()?;
 
-    println!("hello world");
+    // Initialize tracing
+    let _ = Instruments::new()?;
+
+    info!("hello world");
     Ok(())
 }
