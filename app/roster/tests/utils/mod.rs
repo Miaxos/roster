c3375c9a5a3a3d3689692a2c0d6395680971740a
Anthony Griffon <anthony@griffon.one>
2024-01-07 18:41:52 +0100

    tests: enable tests
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- app/roster/tests/utils/mod.rs
+++ app/roster/tests/utils/mod.rs
@@ -5,7 +5,13 @@ mod port_picker;
 
 /// Start a simple Roster server
 pub fn start_simple_server() -> SocketAddr {
-    /*
+    use std::net::{IpAddr, Ipv4Addr};
+    use std::sync::Arc;
+
+    use roster::ServerConfigBuilder;
+
+    use crate::utils::port_picker::pick_unused_port;
+
     let addr = SocketAddr::new(
         IpAddr::V4(Ipv4Addr::LOCALHOST),
         pick_unused_port().unwrap(),
@@ -19,21 +25,22 @@ pub fn start_simple_server() -> SocketAddr {
         server_config.initialize();
     });
     addr
-    */
-    debug_server()
+
+    // debug_server()
 }
 
+/*
 pub fn debug_server() -> SocketAddr {
     SocketAddr::from_str("192.168.64.6:3456").unwrap()
 }
+*/
 
 pub async fn connect_without_auth(
     addr: SocketAddr,
 ) -> redis_async::client::PairedConnection {
-    // use tokio::time::Duration;
+    use tokio::time::Duration;
 
-    // tokio::time::sleep(Duration::from_secs(2)).await;
-
+    tokio::time::sleep(Duration::from_secs(1)).await;
     redis_async::client::paired_connect(addr.ip().to_string(), addr.port())
         .await
         .unwrap()
